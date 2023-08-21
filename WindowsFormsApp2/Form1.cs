a5b69e5eaefeddcb5278cb4a085235fb533d8434
Mert Talha Şimşek <merttalhasimsek@gmail.com>
2023-08-21 17:50:34 +0300

    Update

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using GMap.NET;
 using GMap.NET.WindowsForms;
+using GMap.NET.WindowsPresentation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
@@ -24,6 +26,7 @@ namespace WindowsFormsApp2
             gmap.ShowCenter = false;
             gmap.MinZoom = 1;
             gmap.MaxZoom = 20;*/
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,24 +35,15 @@ namespace WindowsFormsApp2
             gMapControl1.Dock= DockStyle.Fill;
             gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
             gMapControl1.Position = new PointLatLng(39.92390734605342, 32.826400220064734);
-            gMapControl1.Zoom = 15;
-
-            gMapControl1.MouseDown += GMapControl1_MouseDown;
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 1;
+            gMapControl1.IgnoreMarkerOnMouseWheel = true;
 
 
         }
 
-        private void GMapControl1_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left) // Sol tık
-            {
-                gMapControl1.Zoom += 1; // Zoom seviyesini arttır
-            }
-            else if (e.Button == MouseButtons.Right) // Sağ tık
-            {
-                gMapControl1.Zoom -= 1; // Zoom seviyesini azalt
-            }
-        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -59,13 +53,18 @@ namespace WindowsFormsApp2
             {
                 for (int i = (int)gMapControl1.Zoom; i <= gMapControl1.MaxZoom; i++)
                 {
-                    TilePrefetcher obj = new TilePrefetcher();
+                    GMap.NET.TilePrefetcher obj = new GMap.NET.TilePrefetcher();
+                    obj.Icon = this.Icon;
                     obj.Owner = this;
                     obj.ShowCompleteMessage = false;
-                    obj.Start(area, i, gMapControl1.MapProvider, 100,5);
+                    obj.Start(area, i, gMapControl1.MapProvider, 100, 1);
                 }
 
                 Close();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("No Area Chosen", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
